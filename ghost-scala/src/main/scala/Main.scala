e7db0949025e710ed92743206d9e0f5eed0e2e9e
JimmyZhangJW <z1771485029@gmail.com>
2018-11-22 22:43:56 -0800

    ghost location and update function complete

--- ghost-scala/src/main/scala/Main.scala
+++ ghost-scala/src/main/scala/Main.scala
@@ -29,14 +29,28 @@ class Ghost(size: Int) {
 }
 
 class Town(size: Int) {
-  val ghostNumber = scala.util.Random.nextInt(8)+2
-  private var ghosts = new Array[Ghost](ghostNumber);
-  for( a <- 1 until ghostNumber){
-    ghosts(a) = new Ghost(size)
+  private var ghosts = new Array[Ghost](10);
+  var ghostNumber = 0
+  updateGhostLocation()
+
+
+  def updateGhostLocation() {
+    ghostNumber = scala.util.Random.nextInt(8)+2
+    for( a <- 0 until ghostNumber){
+      ghosts(a) = new Ghost(size)
+    }
   }
-//  for( a <- 1 until ghostNumber){
-//    print(s"(${ghosts(a).getX()}, ${ghosts(a).getY()})")
-//  }
+
+  def getGhostLocation(): String = {
+    var output = s"There are $ghostNumber ghosts in town and they are at:\n"
+    for( a <- 0 until ghostNumber){
+      var temp = s"(${ghosts(a).getX()}, ${ghosts(a).getY()})\n"
+      output = output+temp
+    }
+    output
+  }
+
+
 }
 
 
@@ -51,40 +65,42 @@ object Main extends App {
   import system.dispatcher
   import akka.http.scaladsl.server.Directives._
 
-val route = {
-  get {
-    pathSingleSlash {
-      complete("The Ghost Server is up and running, plese use the following commands:\n" +
-        "1. Get the ghost location-> use URI: ghost/location\n" +
-        "2. Update the ghost location in random-> use URI: ghost/update\n" +
-        "3. Calculate the safe path for commuting (for example) from (10, 20) to (20, 30)-> use URI:navi/10/20/20/30")
-    }~
-    pathPrefix("ghost") {
-      get {
-        path("location") {
-          complete("BOOM HERE IS THE LOCATION FOR YOU!")
-        } ~
-          path("update") {
-            complete("BOOM HERE IS THE UPDATE FOR YOU!")
-          }
-      }
-    } ~
-      path("navi" / Remaining) { pathRest =>
-        val pos = raw"(\d{1,3})/(\d{1,3})/(\d{1,3})/(\d{1,3})".r
-        try {
-          val res = pathRest match {
-            case pos(s1, s2, e1, e2) => s"Start navigation for ($s1,$s2) and ($e1, $e2)"
-          }
-          complete(res)
-        } catch {
-          case e: Exception => complete("Please enter the starting/end point in correct form")
+  val route = {
+    get {
+      pathSingleSlash {
+        complete("The Ghost Server is up and running, plese use the following commands:\n" +
+          "1. Get the ghost location-> use URI: ghost/location\n" +
+          "2. Update the ghost location in random-> use URI: ghost/update\n" +
+          "3. Calculate the safe path for commuting (for example) from (10, 20) to (20, 30)-> use URI:navi/10/20/20/30")
+      }~
+      pathPrefix("ghost") {
+        get {
+          path("location") {
+            complete(town.getGhostLocation())
+          } ~
+            path("update") {
+              town.updateGhostLocation()
+              complete("The ghost locations are updated!\n" +
+                town.getGhostLocation())
+            }
         }
       } ~
-      path("crash") {
-        sys.error("BOOM!")
-      }
+        path("navi" / Remaining) { pathRest =>
+          val pos = raw"(\d{1,3})/(\d{1,3})/(\d{1,3})/(\d{1,3})".r
+          try {
+            val res = pathRest match {
+              case pos(s1, s2, e1, e2) => s"Start navigation for ($s1,$s2) and ($e1, $e2)"
+            }
+            complete(res)
+          } catch {
+            case e: Exception => complete("Please enter the starting/end point in correct form")
+          }
+        } ~
+        path("crash") {
+          sys.error("BOOM!")
+        }
+    }
   }
-}
 
   var binding = Http().bindAndHandle(route, host, port)
 
